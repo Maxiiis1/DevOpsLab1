0ee247dc501cbbc79b2bbd925330f7148c622a9f
алисия <aliskondr205@mail.ru>
2025-11-12 20:42:59 +0500

    Add files via upload

---
+++ devops-lab3/go.mod
@@ -0,0 +1,3 @@
+module devops-lab
+
+go 1.23
---
+++ devops-lab3/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import (
+    "fmt"
+    "net/http"
+    "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    name := os.Getenv("APP_NAME")
+    if name == "" {
+        name = "DevOps App"
+    }
+    fmt.Fprintf(w, "Hello from %s!\n", name)
+}
+
+func main() {
+    port := os.Getenv("APP_PORT")
+    if port == "" {
+        port = "8080"
+    }
+    http.HandleFunc("/", handler)
+    fmt.Printf("Server is running on port %s...\n", port)
+    http.ListenAndServe(":"+port, nil)
+}
